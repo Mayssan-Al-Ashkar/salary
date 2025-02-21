3c621cadd064f2845cdf5074eab98c00b84ea432
Mayssan <mayssanalashkar@gmail.com>
2025-02-22 01:38:27 +0200

    second commit: equations

--- nabiha's_salary.py
+++ nabiha's_salary.py
@@ -5,5 +5,20 @@ def main():
     rent = float(input("Enter the percentage for rent: "))
     electricity = float(input("Enter the percentage for electricity: "))
     
+    savingA = (saving / 100) * salary
+    rentA = (rent / 100) * salary
+    electricityA = (electricity / 100) * salary
+    total = savingA + rentA + electricityA
+    remainder = salary - total
+    Yrent = rentA * 12
+    Yelectricity = electricityA *12
+    newSalary = salary ** 2
+
+    if savingA != 0:
+        leftSaving = 50 / savingA
+    else:
+        leftSaving = 0
+
+    
 
     
