2d488b3919c279701c706d4c71d2cdb0c5e90980
Mayssan <mayssanalashkar@gmail.com>
2025-02-22 01:47:45 +0200

    third commit: printing

--- nabiha's_salary.py
+++ nabiha's_salary.py
@@ -18,6 +18,20 @@ def main():
         leftSaving = 50 / savingA
     else:
         leftSaving = 0
+    
+    print("\n Financial Summary for " + month)
+    print("Salary: $" + str(round(salary, 2)))
+    print("Savings: $" + str(round(savings_amount, 2)))
+    print("Rent: $" + str(round(rent_amount, 2)))
+    print("Electricity: $" + str(round(electricity_amount, 2)))
+    print("Total Spent: $" + str(round(total_allocated, 2)))
+    print("Money Left: $" + str(round(remainder, 2)))
+    print("Yearly Rent: $" + str(round(yearly_rent, 2)))
+    print("Yearly Electricity: $" + str(round(yearly_electricity, 2)))
+    print("Salary Squared: " + str(round(salary_squared, 2)))
+    print("Extra $50 divided by savings: " + str(round(savings_division, 2)))
+    print("Savings left after $5 cut: $" + str(round(amount_left_in_savings, 2)) + "\n")
+
 
     
 
