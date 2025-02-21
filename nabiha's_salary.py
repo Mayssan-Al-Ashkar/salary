b58801bd7801b6861f28f274f8792fb4a7dfd53c
Mayssan <mayssanalashkar@gmail.com>
2025-02-22 00:46:04 +0200

    test

---
+++ nabiha's_salary.py
@@ -0,0 +1,9 @@
+def main():
+    salary = float(input("Enter your salary for the month: "))
+    month = input("Enter the month you are storing the salary for: ")
+    saving = float(input("Enter the percentage for savings: "))
+    rent = float(input("Enter the percentage for rent: "))
+    electricity = float(input("Enter the percentage for electricity: "))
+    
+
+    
